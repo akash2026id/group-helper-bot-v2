845802b718142938b3238b28578406135181f24c
akash2026id <ff.akash.99.pro@gmail.com>
2026-03-22 12:58:02 +0600

    Add files via upload

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, send_file, jsonify
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return send_file('index.html')
+
+@app.route('/health')
+def health():
+    return jsonify({'status': 'ok'})
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
